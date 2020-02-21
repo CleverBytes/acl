fd293bf290724dd64d3ace16c132ffb2ac4422e1
Clever Bytes <kcy493@gmail.com>
2020-02-22 01:01:14 +0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -66,16 +66,16 @@ defmodule Acl.MixProject do
     {:ecto, "~> 3.3.3"},
     #{:ecto_sql, "\~> 3.0"},
      # {:jason, "\~> 1.0"},
-     {:plug_cowboy, "\~> 1.0.0"},
+      {:cowboy, "~>  2.7.0"},
+     {:plug_cowboy, "~> 1.0.0"},
       #{:ex_doc, ">= 0.0.0", only: :dev},
-      #{:phoenix, "\~> 1.3.0"},
+      {:phoenix, "~> 1.4.14"},
       #{:phoenix_pubsub, "\~> 1.0"},
       #{:phoenix_ecto, "\~> 3.2"},
       #{:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "\~> 2.13.2"},
-      {:phoenix_live_reload, "\~> 1.0", only: :dev},
+      {:phoenix_html, "~> 2.13.2"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev}
       #{:gettext, "\~> 0.11"},
-      {:cowboy, "~>  2.7.0"}
     ]
   end
 

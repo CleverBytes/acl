6862be96a428775b878b8266ae5eab39e475cc93
Clever Bytes <kcy493@gmail.com>
2020-02-22 00:56:38 +0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -72,7 +72,7 @@ defmodule Acl.MixProject do
       #{:phoenix_pubsub, "\~> 1.0"},
       #{:phoenix_ecto, "\~> 3.2"},
       #{:postgrex, ">= 0.0.0"},
-      #{:phoenix_html, "\~> 2.10"},
+      {:phoenix_html, "\~> 2.13.2"},
       {:phoenix_live_reload, "\~> 1.0", only: :dev},
       #{:gettext, "\~> 0.11"},
       {:cowboy, "~>  2.7.0"}
